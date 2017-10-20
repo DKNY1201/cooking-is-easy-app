52b25489f7f98a17fe34b3a6367e27131417214d
Quy Tran <quytran288@gmail.com>
2017-10-19 23:56:39 -0500

    Working on Recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,8 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Recipe 1', 'Description 1', 'https://trangquynh.files.wordpress.com/2011/02/gc3a0-xc3a9-phay-1.jpg'),
+    new Recipe('Recipe 2', 'Description 2', 'http://mangdoisong.com/Images/Upload/2016/8/Images/' +
+      'cach-nau-chao-long-heo-ngon-de-ban-theo-cach-truyen-thong-cua-nguoi-ha-noi-phan-5.jpg')
+  ];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
   }
